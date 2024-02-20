c7113111c7ca5f80c8383bb36292296b817dd70b
TomasTubertSFC <tomas.tubert@scienceforchange.eu>
2024-02-20 16:33:49 +0100

    Added bootstrap and leave only one theme

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -1,6 +1,6 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { LayoutService } from './service/app.layout.service';
+// import { LayoutService } from './service/app.layout.service';
 
 @Component({
     selector: 'app-menu',
@@ -10,7 +10,7 @@ export class AppMenuComponent implements OnInit {
 
     model: any[] = [];
 
-    constructor(public layoutService: LayoutService) { }
+    // constructor(public layoutService: LayoutService) { }
 
     ngOnInit() {
         this.model = [
