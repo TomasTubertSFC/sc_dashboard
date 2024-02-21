f7d96c107e299a3aea31b196905bbf07ecdb3ec6
TomasTubertSFC <tomas.tubert@scienceforchange.eu>
2024-02-21 11:43:01 +0100

    Clean it all the code at layout.component with the min functionaliti to toggle sidebar menu. Now I have to rename variables

--- src/app/layout/app.layout.component.ts
+++ src/app/layout/app.layout.component.ts
@@ -1,27 +1,22 @@
-import { Component, OnDestroy, Renderer2, ViewChild, signal } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  Renderer2,
+  ViewChild,
+  signal,
+} from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter, Subject, Subscription } from 'rxjs';
-// import { LayoutService } from './service/app.layout.service';
 import { AppSidebarComponent } from './app.sidebar.component';
 import { AppTopBarComponent } from './app.topbar.component';
 
-
- interface AppConfig {
-  inputStyle: string;
-  colorScheme: string;
-  theme: string;
-  ripple: boolean;
+interface AppConfig {
   menuMode: string;
-  scale: number;
 }
 
 interface LayoutState {
   staticMenuDesktopInactive: boolean;
-  overlayMenuActive: boolean;
-  profileSidebarVisible: boolean;
-  configSidebarVisible: boolean;
   staticMenuMobileActive: boolean;
-  menuHoverActive: boolean;
 }
 
 @Component({
@@ -29,12 +24,11 @@ interface LayoutState {
   templateUrl: './app.layout.component.html',
 })
 export class AppLayoutComponent implements OnDestroy {
-
   sidebarMenuIsOpen: boolean = false;
 
   private overlayOpen = new Subject<any>();
 
-    overlayOpen$ = this.overlayOpen.asObservable();
+  overlayOpen$ = this.overlayOpen.asObservable();
 
   overlayMenuOpenSubscription: Subscription;
 
@@ -47,65 +41,53 @@ export class AppLayoutComponent implements OnDestroy {
   @ViewChild(AppTopBarComponent) appTopbar!: AppTopBarComponent;
 
   _config: AppConfig = {
-    ripple: false,
-    inputStyle: 'outlined',
     menuMode: 'static',
-    colorScheme: 'light',
-    theme: 'lara-light-indigo',
-    scale: 14,
-};
+  };
 
   config = signal<any>(this._config);
 
   state: LayoutState = {
-      staticMenuDesktopInactive: false,
-      overlayMenuActive: false,
-      profileSidebarVisible: false,
-      configSidebarVisible: false,
-      staticMenuMobileActive: false,
-      menuHoverActive: false,
+    staticMenuDesktopInactive: false,
+    staticMenuMobileActive: false,
   };
 
   constructor(
-    // public layoutService: LayoutService,
     public renderer: Renderer2,
     public router: Router
   ) {
-    this.overlayMenuOpenSubscription =
-      this.overlayOpen$.subscribe(() => {
-        if (!this.menuOutsideClickListener) {
-          this.menuOutsideClickListener = this.renderer.listen(
-            'document',
-            'click',
-            (event) => {
-              //All the components that triggers hidemenu
-              const isOutsideClicked = !(
-                this.appSidebar.el.nativeElement.isSameNode(event.target) ||
-                this.appSidebar.el.nativeElement.contains(event.target) ||
-                this.appTopbar.menuButton.nativeElement.isSameNode(
-                  event.target
-                ) ||
-                this.appTopbar.menuButton.nativeElement.contains(event.target)
-              );
+    this.overlayMenuOpenSubscription = this.overlayOpen$.subscribe(() => {
+      if (!this.menuOutsideClickListener) {
+        this.menuOutsideClickListener = this.renderer.listen(
+          'document',
+          'click',
+          (event) => {
+            //All the components that triggers hidemenu
+            const isOutsideClicked = !(
+              this.appSidebar.el.nativeElement.isSameNode(event.target) ||
+              this.appSidebar.el.nativeElement.contains(event.target) ||
+              this.appTopbar.menuButton.nativeElement.isSameNode(
+                event.target
+              ) ||
+              this.appTopbar.menuButton.nativeElement.contains(event.target)
+            );
 
-              if (isOutsideClicked) {
-                this.hideMenu();
-              }
+            if (isOutsideClicked) {
+              this.hideMenu();
             }
-          );
-        }
+          }
+        );
+      }
 
-        if (this.state.staticMenuMobileActive) {
-          this.blockBodyScroll();
-        }
-      });
+      if (this.state.staticMenuMobileActive) {
+        this.blockBodyScroll();
+      }
+    });
 
     //If route changes hide menu
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe(() => {
         this.hideMenu();
-        // this.hideProfileMenu();
       });
   }
 
@@ -113,21 +95,18 @@ export class AppLayoutComponent implements OnDestroy {
     this.sidebarMenuIsOpen = !this.sidebarMenuIsOpen;
     if (this.isDesktop()) {
       this.state.staticMenuDesktopInactive =
-          !this.state.staticMenuDesktopInactive;
-  } else {
-      this.state.staticMenuMobileActive =
-          !this.state.staticMenuMobileActive;
+        !this.state.staticMenuDesktopInactive;
+    } else {
+      this.state.staticMenuMobileActive = !this.state.staticMenuMobileActive;
 
       if (this.state.staticMenuMobileActive) {
-          this.overlayOpen.next(null);
+        this.overlayOpen.next(null);
       }
-  }
+    }
   }
 
   hideMenu() {
-    this.state.overlayMenuActive = false;
     this.state.staticMenuMobileActive = false;
-    this.state.menuHoverActive = false;
     if (this.menuOutsideClickListener) {
       this.menuOutsideClickListener();
       this.menuOutsideClickListener = null;
@@ -137,15 +116,7 @@ export class AppLayoutComponent implements OnDestroy {
 
   isDesktop() {
     return window.innerWidth > 991;
-}
-
-  // hideProfileMenu() {
-  //   this.layoutService.state.profileSidebarVisible = false;
-  //   if (this.profileMenuOutsideClickListener) {
-  //     this.profileMenuOutsideClickListener();
-  //     this.profileMenuOutsideClickListener = null;
-  //   }
-  // }
+  }
 
   //Block body scroll while overlay menu is open on mobile
   blockBodyScroll(): void {
@@ -170,19 +141,13 @@ export class AppLayoutComponent implements OnDestroy {
     }
   }
 
-  get containerClass() {//Si comento esto el menú no se esconde
+  //Toggle classes to show or not sidebar menu
+  get containerClass() {
     return {
-      // 'layout-theme-light': this.layoutService.config().colorScheme === 'light',
-      // 'layout-theme-dark': this.layoutService.config().colorScheme === 'dark',
-      'layout-overlay': this.config().menuMode === 'overlay',
-      'layout-static': this.config().menuMode === 'static',
       'layout-static-inactive':
         this.state.staticMenuDesktopInactive &&
         this.config().menuMode === 'static',
-      'layout-overlay-active': this.state.overlayMenuActive,
-      'layout-mobile-active': this.state.staticMenuMobileActive,
-      // 'p-input-filled': this.layoutService.config().inputStyle === 'filled',
-      // 'p-ripple-disabled': !this.layoutService.config().ripple,
+        'layout-mobile-active': this.state.staticMenuMobileActive,
     };
   }
 
