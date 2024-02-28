4e3c773763b8055e4a335bf11c9f1df22ffd79ba
almenarasfc <alberto.almenara@scienceforchange.eu>
2024-02-28 17:18:16 +0100

    los conos aparecen dinamicamente en el mapa en funcion de episodio seleccionado en aln la linea de tiempo

--- src/app/services/episode.service.ts
+++ src/app/services/episode.service.ts
@@ -7,12 +7,19 @@ import { BehaviorSubject, Subject } from 'rxjs';
 })
 export class EpisodeService {
   private _episode: BehaviorSubject<Episode | null> = new BehaviorSubject<Episode | null>(null);
-
   public get episode(): BehaviorSubject<Episode | null> {
     return this._episode;
   }
   public set episode(value: Episode) {
     this._episode.next(value);
+  }
+
+  private _observation: BehaviorSubject<number | null> = new BehaviorSubject<number | null>(null);
+  public get observation(): BehaviorSubject<number | null> {
+    return this._observation;
+  }
+  public set observation(value: number | null) {
+    this._observation.next(value);
   }
 
   constructor() { }
