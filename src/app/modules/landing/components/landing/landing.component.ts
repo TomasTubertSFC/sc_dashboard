b85855e78c3f7670201832a6825a96ee090d7e9a
TomasTubertSFC <tomas.tubert@scienceforchange.eu>
2024-02-29 09:56:43 +0100

    finidesh mock header and company section

--- src/app/modules/landing/components/landing/landing.component.ts
+++ src/app/modules/landing/components/landing/landing.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.component.scss',
 })
 export class LandingComponent {
-
+  company_icons: { src: string; alt: string; id: number }[] = [
+    {
+      src: '../assets/images/icon-fecyt.png',
+      alt: 'icono Fecyt',
+      id: 1,
+    },
+    {
+      src: '../assets/images/logo-ministerio-innovacion.svg',
+      alt: 'ministerio de innovación',
+      id: 2,
+    },
+  ];
 }
