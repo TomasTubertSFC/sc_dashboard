d9696180348ce40f91d199d36883e6063adbd5da
TomasTubertSFC <tomas.tubert@scienceforchange.eu>
2024-05-16 08:15:38 +0200

    Created the filter and it's working

--- src/app/modules/overview/components/bar-chart/bar-chart.component.ts
+++ src/app/modules/overview/components/bar-chart/bar-chart.component.ts
@@ -1,9 +1,8 @@
-import { AfterViewInit, Component, OnInit, inject } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit, inject } from '@angular/core';
 import * as echarts from 'echarts/core';
 import { GridComponent, GridComponentOption } from 'echarts/components';
 import { BarChart, BarSeriesOption } from 'echarts/charts';
 import { CanvasRenderer } from 'echarts/renderers';
-import { HttpClient } from '@angular/common/http';
 import { ObservationsService } from '../../../../services/observations/observations.service';
 import { Observations } from '../../../../models/observations';
 import { lastValueFrom } from 'rxjs';
@@ -25,23 +24,53 @@ interface ObservationsDataChart {
   styleUrl: './bar-chart.component.scss',
 })
 export class BarChartComponent implements OnInit, AfterViewInit {
-  myChart!: echarts.ECharts;
-  options: EChartsOption;
-  observations: ObservationsDataChart[] = [];
-  today: Date = new Date();
-  lastDay30: Date = new Date(new Date().setDate(this.today.getDate() - 30));
+  private myChart!: echarts.ECharts;
+  private options: EChartsOption;
+  private observations: ObservationsDataChart[] = [];
+  private obsFiltered: ObservationsDataChart[] = [];
+  public today: Date = new Date();
+  private lastDay30: Date = new Date(new Date().setDate(this.today.getDate() - 30));
 
-  observationService: ObservationsService = inject(ObservationsService);
+  private observationService: ObservationsService = inject(ObservationsService);
 
   public filtersForm: FormGroup = new FormGroup({
     daysFilter: new FormControl([this.lastDay30, new Date()], []),
   });
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.myChart.resize();
+  }
+
   ngOnInit(): void {
     echarts.use([GridComponent, BarChart, CanvasRenderer]);
     this.filtersForm.valueChanges.subscribe(
       (values: { daysFilter: [Date, Date | null] }) => {
-        console.log('values', values);
+        const haveTwoDaysSelected = values.daysFilter[1] !== null;
+        if (haveTwoDaysSelected) {
+          this.obsFiltered = this.observations.filter((obs) => {
+            const isBeforeToday = obs.date <= values.daysFilter[1];
+            const isAfterLastDay30 = obs.date >= values.daysFilter[0];
+            if (isBeforeToday && isAfterLastDay30) return true;
+            return false;
+          });
+          this.options = {
+            xAxis: {
+              type: 'category',
+              data: this.obsFiltered.map((obs) => obs.date.toDateString()),
+            },
+            yAxis: {
+              type: 'value',
+            },
+            series: [
+              {
+                data: this.obsFiltered.map((obs) => obs.count),
+                type: 'bar',
+              },
+            ],
+          };
+          this.options && this.myChart.setOption(this.options);
+        }
       }
     );
   }
@@ -81,7 +110,6 @@ export class BarChartComponent implements OnInit, AfterViewInit {
       {}
     );
 
-    console.log('arrGroupObsByDays', arrGroupObsByDays);
     const arrSorted = Object.values(arrGroupObsByDays)
       .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
       .map((obs) => ({
@@ -92,7 +120,7 @@ export class BarChartComponent implements OnInit, AfterViewInit {
     this.observations = arrSorted;
     const arr30DaysBefore = arrSorted.filter((obs) => {
       const isBeforeToday = obs.date <= this.today;
-      const isAfterLastDay30 = obs.date >= this.lastDay30 ;
+      const isAfterLastDay30 = obs.date >= this.lastDay30;
       if (isBeforeToday && isAfterLastDay30) return true;
       return false;
     });
@@ -105,12 +133,31 @@ export class BarChartComponent implements OnInit, AfterViewInit {
     this.myChart = echarts.init(chartDom);
     this.myChart.showLoading();
     const obs = await this.getAllObservationsAndFormated();
-
-    console.log('obs', obs);
+    const dateOptions: Intl.DateTimeFormatOptions = {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    };
     this.options = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross'
+        },
+        formatter: function (params:any) {
+          console.log('params', params)
+          return params[0].data +' ';
+        }
+      },
       xAxis: {
         type: 'category',
-        data: obs.map((obs) => obs.date.toDateString()),
+        data: obs.map((obs) =>
+          obs.date.toLocaleDateString('ca-Es', dateOptions)
+        ),
+        axisLabel: {
+          interval: 0, // This forces displaying all labels
+          rotate: 45, // Optional: you can rotate labels to prevent overlapping
+        },
       },
       yAxis: {
         type: 'value',
