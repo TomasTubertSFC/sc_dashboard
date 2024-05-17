e31d6290956d5f46be1b058ea27e18ad7e2cfd1a
TomasTubertSFC <tomas.tubert@scienceforchange.eu>
2024-05-17 09:23:33 +0200

    Deleted broken import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { AuthGuard } from './guards/auth.guard';
 import { RecoverPasswordComponent } from './modules/recover-password/components/recover-password/recover-password.component';
 import { CreatePasswordComponent } from './modules/recover-password/components/create-password/create-password.component';
 import { ProfileComponent } from './modules/profile/components/profile/profile.component';
-import { LandingComponent } from './modules/landing/components/landing/landing.component';
 import { MapComponent } from './modules/map/page/map.component';
 import { OverviewComponent } from './modules/overview/page/overview/overview.component';
 
