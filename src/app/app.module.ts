e1855e6d97f39c76ea9431296d5d4f7394b85dec
almenarasfc <alberto.almenara@scienceforchange.eu>
2024-06-17 10:30:13 +0200

    tranducciones instaladas y funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppLayoutModule } from './layout/app.layout.module';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedComponentsModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { authInterceptorProviders } from './interceptor/auth.interceptor';
 import { initializeInterceptorProvider } from './helpers/intializeApp';
 import { RecoverPasswordModule } from './modules/recover-password/recover-password.module';
@@ -14,7 +14,13 @@ import { MapModule } from './modules/map/map.module';
 import { LoginModule } from './modules/login/login.module';
 import { MessageService } from 'primeng/api';
 import { OverviewModule } from './modules/overview/overview.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { environment } from '../environments/environments';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -31,6 +37,14 @@ import { OverviewModule } from './modules/overview/overview.module';
     HttpClientXsrfModule.withOptions({
       cookieName: 'XSRF-TOKEN',
       headerName: 'X-XSRF-TOKEN',
+    }),
+    TranslateModule.forRoot({
+      defaultLanguage: environment.DEFAULT_LANGUAGE,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
     }),
   ],
   providers: [
